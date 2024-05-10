64a89a296fdc228a49ab2ac20ed9ae10c8e7b31a
Darina Ivashchenko <kittswolves@gmail.com>
2024-05-10 10:26:24 +0300

    fix task 6

--- js/task-6.js
+++ js/task-6.js
@@ -12,21 +12,19 @@ const divEl = document.querySelector("#boxes");
 creBtn.addEventListener("click", createBoxes);
 desBtn.addEventListener("click", destroyBoxes);
 
-function boxesMarkUp(counter) {
-  const box = document.createElement("div");
-  box.classList.add("sixth-box");
-  box.style.width = `${30 + counter * 10}px`;
-  box.style.height = `${30 + counter * 10}px`;
-  box.style.backgroundColor = getRandomHexColor();
-  divEl.append(box);
-}
-
 function createBoxes() {
+  const boxArray = [];
   if (inputEl.value > 0 && inputEl.value <= 100) {
     for (let i = 0; i < inputEl.value; i++) {
-      boxesMarkUp(i);
+        const box = document.createElement("div");
+        box.classList.add("sixth-box");
+        box.style.width = `${30 + i * 10}px`;
+        box.style.height = `${30 + i * 10}px`;
+        box.style.backgroundColor = getRandomHexColor();
+        boxArray.push(box);
     }
   }
+  divEl.append(...boxArray);
   inputEl.value = "";
 }
 
